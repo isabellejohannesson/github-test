6cedf5335996b5a7b0e488059cdca8f1f0b9f223
Isabelle Johannesson <isabelle.johannesson@outlook.com>
2023-02-15 14:37:45 +0100

    testing deploy with railway

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="sv">
+
+<head>
+    <title>Deploy Test</title>
+    <meta charset="uft-8">
+</head>
+
+<body>
+    <h1>Testar att deploya en Laravel-app!</h1>
+</body>
+
+</html>
